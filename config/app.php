bf6846598bafa24322ba02bfbb38385a9809e712
Weslley Ribeiro <wess_ribeiro@hotmail.com>
2019-03-03 14:11:46 -0300

    incluindo imports para xls com o packge maatwebsite

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -174,7 +174,6 @@ return [
         // Leroy\Providers\BroadcastServiceProvider::class,
         Leroy\Providers\EventServiceProvider::class,
         Leroy\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
@@ -225,7 +224,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
